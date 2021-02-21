900becee3b7b42f80e384d59bd2f5e97698e3d9f
awmelia <56584822+awmelia@users.noreply.github.com>
2021-02-21 14:44:15 +0100

    Removed classes
    Because I changed between computers and operating-environments the github version got corrupted. Had achieved so little so decided to change my approach altogether. Therefore the old project was replaced.

---
+++ GainsApp/MuscleTrackerApp/DataAccessLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MuscleTrackerApp
+{
+    class DataAccessLayer
+    {
+        private SqlConnection sqlConnection;
+        private string connectionString = "Server=localhost;Database=Smart Office 2.0;User Id = user2.0; Password = losen";
+
+        public SqlConnection SqlConnection { get => SqlConnection; set => SqlConnection = value; }
+
+
+    }
+}
---
+++ GainsApp/MuscleTrackerApp/MuscleForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuscleTrackerApp
+{
+    public partial class MuscleForm : Form
+    {
+        public MuscleForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
