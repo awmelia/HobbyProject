1b7bce5c397b0ffc9690cc798b8ad23aef972b61
awmelia <56584822+awmelia@users.noreply.github.com>
2021-02-21 15:47:52 +0100

    GetExercises
    Finished some of the code and the SQL-database has also been created

--- GainsApp/MuscleTrackerApp/DataAccessLayer.cs
+++ GainsApp/MuscleTrackerApp/DataAccessLayer.cs
@@ -11,8 +11,51 @@ namespace MuscleTrackerApp
     {
         private SqlConnection sqlConnection;
         private string connectionString = "Server=localhost;Database=Smart Office 2.0;User Id = user2.0; Password = losen";
-
+        
         public SqlConnection SqlConnection { get => SqlConnection; set => SqlConnection = value; }
+
+        /*****************.
+           *  Function             closeConnection()
+           *   Description         Method that closes the SqlConnection object of this instance.
+           *    Parameters
+           *     Returns           
+           ***********/
+        public void CloseConnection()
+        {
+            sqlConnection.Close();
+        }
+
+        /*****************.
+    *  Function             getExercises
+    *   Description         Method that, if successful, returns an sqlDataReader with all associated information from database
+    *    Parameters
+    *     Returns           SqlDataReader
+    ***********/
+
+        public SqlDataReader GetExercises()
+        {
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Exercise", sqlConnection);
+
+            try
+            {
+                sqlConnection.Open();
+                SqlDataReader dataReader = sqlCommand.ExecuteReader();
+                return dataReader;
+            }
+            catch (SqlException e)
+            {
+
+                throw e;
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+
+        }
 
 
     }
